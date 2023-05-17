f63a8f163ea7fbe10ad1944e7b0f0a1082dcf083
Assaf Tal <assaftal391@gmail.com>
2023-05-17 21:37:18 +0300

    feet:add api routes

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import argparse
+from api.api import app
+
+def menu():
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--port",type=int,default=5000,help='Port For API')
+    args=parser.parse_args()
+    return args
+
+def main(port):
+    app.run(host='localhost',port=port)
 
 
-
+if __name__=="__main__":
+    args = menu()
+    main(port=args.port)
