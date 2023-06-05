53ad18fe5decd13b436fb1f67018e5214800105e
Yossi Hananya <yossiha93@gmail.com>
2023-06-05 12:20:47 +0300

    feat: add debug mode for flask app

--- main.py
+++ main.py
@@ -3,14 +3,27 @@ from api.api import app
 
 def menu():
     parser=argparse.ArgumentParser()
-    parser.add_argument("--port",type=int,default=5000,help='Port For API')
-    args=parser.parse_args()
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=5000,
+        help='Port For API'
+    )
+    
+    parser.add_argument(
+        "-d",
+        "--debug", 
+        action="store_true",
+        help='Activate debug mode, code changes will automatically updated in the browser'
+    )
+    args = parser.parse_args()
+    
     return args
 
-def main(port):
-    app.run(host='localhost',port=port)
+def main(port, debug):
+    app.run(host='localhost',port=port, debug=debug)
 
 
 if __name__=="__main__":
-    args = menu()
-    main(port=args.port)
+    args = vars(menu())
+    main(**args)
