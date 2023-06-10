443cf997aecaa3f0adae3ff586d6b7bdde7975fc
Assaf Tal <assaftal391@gmail.com>
2023-06-10 19:12:11 +0300

    Adding Tests

--- api/api.py
+++ api/api.py
@@ -1,19 +1,44 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
+import json
     
 app=Flask(__name__)
 
 
-players_list=[
-    { 'name': 'Assaf', 'team': 1 }
+# An array that will hold players
+players = [
+    {
+        "id": 1,
+        "first_name": "Assaf",
+        "last_name": "Tal",
+    },
+    {
+        "id": 2,
+        "first_name": "Yossi",
+        "last_name": "Hananya",
+    }
 ]
 
-@app.route('/players')
-def get_players():
-    return jsonify(players_list)
+@app.route("/")
+def index():
+    return "Testing, Flask!"
 
-@app.route('/players',methods=['POST'])
-def register_player():
-    player_data=request.get_json()
-    players_list.append(player_data)
-    response = {'message': 'Player added successfully'}
-    return jsonify(response), 200
+@app.route('/playersapi/players')
+#Getting all players
+def get_players():
+    return {"Players":players}, 200
+
+@app.route('/playersapi/players',methods=['POST'])
+#Adding a new player to the list
+def add_player():
+    new_player = request.get_data() 
+    players.append(json.loads(new_player.decode()))
+    return jsonify({"message": "Player added successfully"}),201
+
+@app.route("/playersapi/players/<int:player_id>", methods=['GET'])
+def get_by_id(player_id:int):
+    #Getting player information by ID
+    for player in players:
+        if player['id']==player_id:
+            return jsonify({"Player_Details": player}),200
+    return 
+500
---
+++ tests/test_routes.py
@@ -0,0 +1,49 @@
+def test_home_page(client):
+    """
+    GIVEN a Flask API Application Is Running
+    WHEN The '/' Page Is Requested (GET)
+    THEN Check The Response Is Valid 
+    """
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Testing, Flask!" in response.data
+
+def test_playersapi_players_route(client):
+    """
+    GIVEN a Flask API Application Is Running 
+    WHEN The '/playeraapi/players' Page Is Requested (GET)
+    THEN Check The Response Is Valid 
+    """
+    response = client.get('/playersapi/players')
+    assert response.status_code == 200
+    assert response.json['Players'][0]['first_name']=='Assaf'
+    assert response.json['Players'][1]['id']==2
+
+def test_playersapi_players_route_post_data(client):
+    """
+    GIVEN a Flask API Application Is Running 
+    WHEN The '/playeraapi/players' Page Is Send POST Requeste With Data
+    THEN Check The Response Is Valid 
+    """
+    response = client.post('/playersapi/players',json={"first_name":"Tal","id":3,"last_name":'Bitton'})
+    assert response.status_code == 201
+    assert response.json["message"] == "Player added successfully"
+
+def test_playersapi_players_by_id(client):
+    """
+    GIVEN a Flask API Application Is Running 
+    WHEN The '/playeraapi/players/(number)' Is Requested (GET)
+    THEN Check The Response Is Valid 
+    """
+    response = client.get('/playersapi/players/1')
+    assert response.status_code == 200
+    assert response.json["Player_Details"]["id"]==1
+
+def test_playersapi_players_by_id_that_not_in_list(client):
+    """
+    GIVEN a Flask API Application Is Running 
+    WHEN The '/playeraapi/players/(number not in list)' Is Requested (GET)
+    THEN Check The Response Is Valid 
+    """
+    response = client.get('/playersapi/players/8')
+    assert response.status_code == 500
