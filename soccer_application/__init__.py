a63e3436826f8f7d247b76a2faa4ab2c5bb5ad6e
Yossi Hananya <yossiha93@gmail.com>
2023-06-14 16:08:47 +0300

    fix: db configuration

--- soccer_application/__init__.py
+++ soccer_application/__init__.py
@@ -2,5 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
+app.app_context().push()
 db=SQLAlchemy(app)
