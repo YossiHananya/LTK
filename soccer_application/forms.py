9da3552ae906a3d343da5d938b0d5026945f6c01
Assaf Tal <assaftal391@gmail.com>
2023-06-17 19:42:59 +0300

    Fixing imports

---
+++ soccer_application/forms.py
@@ -0,0 +1,51 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField,SubmitField,BooleanField
+from wtforms.validators import DataRequired,Length,Email,ValidationError,validators,EqualTo
+import re
+
+def check_password_strength(form, field):
+    """
+    Verify the strength of 'password'
+    Returns a dict indicating the wrong criteria
+    A password is considered strong if:
+        8 characters length or more
+        1 digit or more
+        1 symbol or more
+        1 uppercase letter or more
+        1 lowercase letter or more
+    """
+    password = field.data
+    #Checking for number in password
+    if re.search(r"\d",password) is None:
+        raise validators.ValidationError("Password must contain at least one number")
+    #Checking for uppercase in password
+    if re.search(r"[A-Z]", password) is None:
+        raise validators.ValidationError("Password must contain at least one uppercase")
+    #Checking for uppercase in password
+    if re.search(r"[a-z]", password) is None:
+        raise validators.ValidationError("Password must contain at least one lowecase")
+    #Checking for symbol in password
+    if re.search(r"\W", password) is None:
+        raise validators.ValidationError("Password must contain at least one symbol")
+    
+
+class RegistrationForm(FlaskForm):
+
+    username=StringField('Username',
+                         validators=[DataRequired(),Length(min=2,max=20)])
+    email=StringField('Email',
+                      validators=[DataRequired(),Email()])
+    password=PasswordField('Password',
+                           validators=[DataRequired(),Length(min=8),check_password_strength])
+    password_confirmation=PasswordField('Confirm Password',
+                                        validators=[DataRequired(),EqualTo('password')]) 
+    submit=SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email=StringField('Email',
+                      validators=[DataRequired(),Email()])
+    password=PasswordField('Password',
+                        validators=[DataRequired()])
+    remember=BooleanField('Remember Me')
+    submit=SubmitField('Login')
+    
