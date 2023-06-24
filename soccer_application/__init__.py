283d93b8f61d10b37285c604bd1b9c479999c7c8
Assaf Tal <assaftal391@gmail.com>
2023-06-24 16:14:35 +0300

    Adding Yaml Files For Configurations

--- soccer_application/__init__.py
+++ soccer_application/__init__.py
@@ -3,23 +3,31 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from config import get_config
+from .config.config import Config
+import os
 
 db=SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 
-def create_app():
+def create_app(config_name):
     app = Flask(__name__)
-    config=get_config()
-    app.config.from_object(config)
+    config_dir = os.path.join(os.path.dirname(__file__), 'config')
+    if config_name == 'dev':
+        config_file = os.path.join(config_dir, 'dev.yaml')
+    elif config_name == 'testing':
+       config_file = os.path.join(config_dir, 'testing.yaml')
+    else:
+        raise ValueError(f"Invalid configuration name {config_name}")
+    
+    config=Config(config_file)
+    db_path = path.join(path.dirname(__file__), config.get('APP_CONFIG')['DATABASE_URI'])
+    app.config.update(config.get('APP_CONFIG'))
+    app.config['SQLALCHEMY_DATABASE_URI']= f'sqlite:///{db_path}'
 
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
-
-    db_path = path.join(path.dirname(__file__), app.config['SQLALCHEMY_DATABASE_URI'])
-    app.config['SQLALCHEMY_DATABASE_URI']= f'sqlite:///{db_path}'
 
     from soccer_application import api
 
--- soccer_application/config/dev_config.py
+++
@@ -1,6 +0,0 @@
-from .base_config import BaseConfig
-
-class DevConfig(BaseConfig):
-
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'dev_database.db'
