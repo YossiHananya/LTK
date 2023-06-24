f7e7aaac01d88962586ef2f66f86386499fd8ee5
Assaf Tal <assaftal391@gmail.com>
2023-06-24 18:46:48 +0300

    adding ports for configuration files

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
 import argparse
 from soccer_application import create_app
 
-def menu():
-    parser=argparse.ArgumentParser()
-    parser.add_argument(
-        "--port",
-        type=int,
-        default=5000,
-        help='Port For API'
-    )
-    args = parser.parse_args()
-    
-    return args
-
-def main(port):
-    app=create_app('dev')
-    app.run(host='localhost',port=port)
-
 
 if __name__=="__main__":
-    args = vars(menu())
-    main(**args)
+    app=create_app('dev')
+    app.run(host='localhost',port=app.config.get('PORT'))
