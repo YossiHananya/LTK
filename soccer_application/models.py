e873b69c7379db9905941a7bffaad1f4e07134b6
Assaf Tal <assaftal391@gmail.com>
2023-09-13 16:19:56 +0300

    Refactoring DB Models

--- soccer_application/models.py
+++ soccer_application/models.py
@@ -1,91 +1,117 @@
 from flask_login import UserMixin
 from soccer_application import db, login_manager
+from datetime import datetime
 
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id)) 
+    return Users.query.get(int(user_id)) 
 
 
-event_player_association = db.Table('event_player_association',
-    db.Column('event_id', db.Integer, db.ForeignKey('event.id')),
-    db.Column('player_id', db.Integer, db.ForeignKey('player.id'))
+players_in_games = db.Table('player_in_games',
+    db.Column('game_id', db.Integer, db.ForeignKey('games.id')),
+    db.Column('player_id', db.Integer, db.ForeignKey('players.id')),
+    db.Column('created_at', db.DateTime),
+    db.Column('participate', db.Boolean)
 )
 
-team_player_association = db.Table('team_player_association',
-    db.Column('team_id', db.Integer, db.ForeignKey('team.id')),
-    db.Column('player_id', db.Integer, db.ForeignKey('player.id'))
+fans = db.Table('fans',
+    db.Column('player_id', db.Integer, db.ForeignKey('players.id')),
+    db.Column('fav_team_id', db.Integer, db.ForeignKey('favoriteteams.id'))
 )
 
-team_manager_association = db.Table('team_manager_association',
-    db.Column('team_id', db.Integer, db.ForeignKey('team.id')),
-    db.Column('manager_id', db.Integer, db.ForeignKey('manager.id'))
-)
-
-team_location_association = db.Table('team_location_association',
-    db.Column('team_id', db.Integer, db.ForeignKey('team.id')),
-    db.Column('location_id', db.Integer, db.ForeignKey('location.id'))
-)
-
-class Event(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    event_date=db.Column(db.DateTime,nullable=False)
-    players=db.relationship('Player',secondary=event_player_association,backref='events')
-    location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=False)
+class Games(db.Model):
+    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    game_date=db.Column(db.DateTime,nullable=False)
+    team_id=db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
+    court_id=db.Column(db.Integer, db.ForeignKey('courts.id'), nullable=False)
     
     def __repr__(self):
-        return f"Event(location_id: '{self.location_id}','{'at'}','{self.event_date}')"
+        return f"Game(game_id: '{self.id}','{'at'}','{self.game_date}')"
 
-
-class Location(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String, nullable=False, unique=True)
-    events=db.relationship('Event', backref='locations')
+class FavoriteTeams(db.Model):
+    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    team_name=db.Column(db.String(50))
+    country=db.Column(db.String(50))
     
     def __repr__(self):
-        return f"Location('{self.name})"
+        return f"Favorite Team('{self.team_name}')"
 
 
-class Team(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String,nullable=False)
-    players=db.relationship('Player',secondary=team_player_association,backref='teams')
-    locations=db.relationship('Location',secondary=team_location_association, backref='locations')
+class Courts(db.Model):
+    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name=db.Column(db.String(100),nullable=False)
+    address=db.Column(db.String(100),nullable=False)
+
+    def __repr__(self):
+        return f"Court(court_name: '{self.name}','{'at'}','{self.address}')"
+
+
+class Teams(db.Model):
+    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name=db.Column(db.String(100),nullable=False)
+    players=db.relationship('Players', backref='teams', lazy=True)
     
     def __repr__(self):
         return f"Team('{self.name})"
-
-class MainTeam(Team):
-    __tablename__ = "mainteam"
     
-    id=db.Column(db.Integer, db.ForeignKey('team.id'), primary_key=True)
-    team = db.relationship('Team', backref='main_team')
-    players=db.relationship('Player', backref='main_teams')
+class Expenses(db.Model):
+    __table_args__ = (
+        db.CheckConstraint(db.amount >= 0, name='check_amount_positive')
+    )
+    id=db.Column(db.Integer, primary_key=True, autoincerement=True)
+    date=db.Column(db.DateTime)
+    amount=db.Column(db.Integer)
+    description=db.Column(db.String(200))
+    team_id=db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
 
-class User(db.Model, UserMixin):
-    id=db.Column(db.Integer, primary_key=True)
-    username=db.Column(db.String,nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+class Payments(db.Model):
+    __table_args__ = (
+        db.CheckConstraint(db.amount >= 0, name='check_amount_positive')
+    )
+    id=db.Column(db.Integer, primary_key=True, autoincerement=True)
+    date=db.Column(db.DateTime)
+    amount=db.Column(db.Integer)
+    team_id=db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
+    player_id=db.Column(db.Integer, db.ForeignKey('players.id'), nullable=False)
+
+class Users(db.Model, UserMixin):
+    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    created_at=db.Column(db.DateTime, default=datetime.datetime.now())
+    updated_at=db.Column(db.DateTime, nullable=True)
+    username=db.Column(db.String(50),unique=True,nullable=False)
+    password=db.Column(db.String(50), nullable=False)
+    contact_info = db.relationship('ContactInfo', backref='user', lazy=True, uselist=False)
+    #profile_image=db.Column(db.String(200),default='#path to default jpeg.')
         
     def __repr__(self):
-        return f"User('{self.username}','{self.email}')"
+        return f"User('{self.username}')"
+    
+class ContactInfo(db.Model):
+    __table_args__=(
+         db.CheckConstraint('NOT(phone IS NULL AND email IS NULL)')
+    )
+
+    id=db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    phone=db.Column(db.String(25))
+    email=db.Column(db.String(40))
 
 
-class Manager(User):
-    id=db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    teams=db.relationship('Team',secondary=team_manager_association, backref='managers')
+class Managers(Users):
+    id=db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    team_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
         
     def __repr__(self):
         return f"Manager('{self.id}')"
 
-class Player(User):
-    id=db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)    
-    first_name=db.Column(db.String,nullable=False)
-    last_name=db.Column(db.String,nullable=False)
-    gender=db.Column(db.String,nullable=True)
-    main_team_id = db.Column(db.Integer, db.ForeignKey('mainteam.id'), nullable=False)
-    #image_file=db.Column(db.String(20),nullabale=False,default='default.jpg')
+class Player(Users):
+    id=db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    first_name=db.Column(db.String(50),nullable=False)
+    last_name=db.Column(db.String(50),nullable=False)
+    date_of_birth=db.Column(db.DateTime,nullable=False)
+    status=db.Column(db.Boolean,nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
+    #profile_image=db.Column(db.String(20),nullabale=False,default='default.jpg')
 
     def __repr__(self):
             return f"Player('{self.first_name}',' ','{self.last_name}')"
