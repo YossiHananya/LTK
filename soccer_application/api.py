8ba1dc6c86cfeeb3a39e62b24ec7616e1b21afde
Yossi Hananya <yossiha93@gmail.com>
2023-10-01 19:02:12 +0300

    feat: players dashboard

--- soccer_application/api.py
+++ soccer_application/api.py
@@ -8,28 +8,12 @@ from soccer_application.models import Player, User
 @app.route("/")
 @app.route("/home")
 def index():
-    return render_template('home.html', title='home', players=Player.query.all())
+    return render_template('home.html', title='home')
 
-@app.route('/playersapi/players')
-#Getting all players
+@app.route('/players/')
 def get_players():
-    return {"Players":players}, 200
+    return render_template('players.html', title='Dashboard - Players', players=Player.query.all())
 
-@app.route('/playersapi/players',methods=['POST'])
-#Adding a new player to the list
-def add_player():
-    new_player = request.get_data() 
-    players.append(json.loads(new_player.decode()))
-    return jsonify({"message": "Player added successfully"}),201
-
-@app.route("/playersapi/players/<int:player_id>", methods=['GET'])
-def get_by_id(player_id:int):
-    #Getting player information by ID
-    for player in players:
-        if player['id']==player_id:
-            return jsonify({"Player_Details": player}),200
-    return 
-500
 
 @app.route("/register",methods=['GET','POST'])
 def register():
