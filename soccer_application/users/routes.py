0db82d5bf9f2a5351eeb34ac85e1f96f918a7739
Yossi Hananya <yossiha93@gmail.com>
2023-10-02 00:26:45 +0300

    refactor: split api into blueprints

--- soccer_application/users/routes.py
+++ soccer_application/users/routes.py
@@ -1,40 +1,15 @@
-import json
-from flask import jsonify, request, render_template, url_for, flash, redirect, current_app as app
+from flask import Blueprint, request, render_template, url_for, flash, redirect
 from flask_login import login_user, logout_user, current_user, login_required
-from soccer_application import db, bcrypt
-from soccer_application.forms import RegistrationForm, LoginForm, UpdateProfileForm
-from soccer_application.models import Player, User
+from .forms import RegistrationForm, LoginForm, UpdateProfileForm
+from .. import db, bcrypt
+from ..models import User
 
-@app.route("/")
-@app.route("/home")
-def index():
-    return render_template('home.html', title='home', players=Player.query.all())
+users = Blueprint('users', __name__)
 
-@app.route('/playersapi/players')
-#Getting all players
-def get_players():
-    return {"Players":players}, 200
-
-@app.route('/playersapi/players',methods=['POST'])
-#Adding a new player to the list
-def add_player():
-    new_player = request.get_data() 
-    players.append(json.loads(new_player.decode()))
-    return jsonify({"message": "Player added successfully"}),201
-
-@app.route("/playersapi/players/<int:player_id>", methods=['GET'])
-def get_by_id(player_id:int):
-    #Getting player information by ID
-    for player in players:
-        if player['id']==player_id:
-            return jsonify({"Player_Details": player}),200
-    return 
-500
-
-@app.route("/register",methods=['GET','POST'])
+@users.route("/register",methods=['GET','POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
         
     form=RegistrationForm()
 
@@ -50,11 +25,11 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
 
         login_user(user, remember=form.remember.data)
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     
     return render_template('register.html',title='Register',form=form)
 
-@app.route("/login",methods=['GET','POST'])
+@users.route("/login",methods=['GET','POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -69,17 +44,17 @@ def login():
         else:
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('index'))
+            return redirect(next_page) if next_page else redirect(url_for('main.index'))
         
     return render_template('login.html',title='Login',form=form)
 
-@app.route("/logout")
+@users.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('main.index'))
 
 
-@app.route("/profile", methods=['GET', 'POST'])
+@users.route("/profile", methods=['GET', 'POST'])
 @login_required
 def profile():
     form = UpdateProfileForm()
@@ -89,7 +64,7 @@ def profile():
         db.session.commit()
         flash("Profile was updated successfully", 'success')
 
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
 
     if request.method == 'GET':
         form.username.data = current_user.username
