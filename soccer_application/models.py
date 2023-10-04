9419d21aa6735658b2b3aefa1aa08db209f1a0e7
Assaf Tal <assaftal391@gmail.com>
2023-10-04 20:43:11 +0300

    refactor: Change DB Schema And Tests

--- soccer_application/models.py
+++ soccer_application/models.py
@@ -1,6 +1,7 @@
 from flask_login import UserMixin
 from soccer_application import db, login_manager
 from datetime import datetime
+from sqlalchemy import CheckConstraint
 
 
 @login_manager.user_loader
@@ -9,15 +10,15 @@ def load_user(user_id):
 
 
 players_in_games = db.Table('player_in_games',
-    db.Column('game_id', db.Integer, db.ForeignKey('games.id')),
-    db.Column('player_id', db.Integer, db.ForeignKey('players.id')),
+    db.Column('game_id', db.Integer, db.ForeignKey('games.id',ondelete='CASCADE'), primary_key=True),
+    db.Column('player_id', db.Integer, db.ForeignKey('players.id', ondelete='CASCADE'), primary_key=True),
     db.Column('created_at', db.DateTime),
     db.Column('participate', db.Boolean)
 )
 
 fans = db.Table('fans',
-    db.Column('player_id', db.Integer, db.ForeignKey('players.id')),
-    db.Column('fav_team_id', db.Integer, db.ForeignKey('favoriteteams.id'))
+    db.Column('player_id', db.Integer, db.ForeignKey('players.id',ondelete='CASCADE'), primary_key=True),
+    db.Column('favoriteteam_id', db.Integer, db.ForeignKey('favoriteteams.id',ondelete='CASCADE'), primary_key=True)
 )
 
 class Games(db.Model):
@@ -25,11 +26,12 @@ class Games(db.Model):
     game_date=db.Column(db.DateTime,nullable=False)
     team_id=db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
     court_id=db.Column(db.Integer, db.ForeignKey('courts.id'), nullable=False)
+
     
     def __repr__(self):
         return f"Game(game_id: '{self.id}','{'at'}','{self.game_date}')"
 
-class FavoriteTeams(db.Model):
+class Favoriteteams(db.Model):
     id=db.Column(db.Integer, primary_key=True, autoincrement=True)
     team_name=db.Column(db.String(50))
     country=db.Column(db.String(50))
@@ -43,6 +45,8 @@ class Courts(db.Model):
     name=db.Column(db.String(100),nullable=False)
     address=db.Column(db.String(100),nullable=False)
 
+    games=db.relationship('Games', backref='court', lazy=True)
+
     def __repr__(self):
         return f"Court(court_name: '{self.name}','{'at'}','{self.address}')"
 
@@ -50,16 +54,20 @@ class Courts(db.Model):
 class Teams(db.Model):
     id=db.Column(db.Integer, primary_key=True, autoincrement=True)
     name=db.Column(db.String(100),nullable=False)
-    players=db.relationship('Players', backref='teams', lazy=True)
+
+    players=db.relationship('Players', backref='team', lazy=True)
+    payments=db.relationship('Payments', backref='team', lazy=True)
+    expenses=db.relationship('Expenses', backref='team', lazy=True)
+    games=db.relationship('Games', backref='team', lazy=True)
     
     def __repr__(self):
         return f"Team('{self.name})"
     
 class Expenses(db.Model):
     __table_args__ = (
-        db.CheckConstraint(db.amount >= 0, name='check_amount_positive')
+        CheckConstraint('amount >= 0', name='check_amount_positive'),
     )
-    id=db.Column(db.Integer, primary_key=True, autoincerement=True)
+    id=db.Column(db.Integer, primary_key=True)
     date=db.Column(db.DateTime)
     amount=db.Column(db.Integer)
     description=db.Column(db.String(200))
@@ -67,17 +75,17 @@ class Expenses(db.Model):
 
 class Payments(db.Model):
     __table_args__ = (
-        db.CheckConstraint(db.amount >= 0, name='check_amount_positive')
+        CheckConstraint('amount >= 0', name='check_amount_positive'),
     )
-    id=db.Column(db.Integer, primary_key=True, autoincerement=True)
+    id=db.Column(db.Integer, primary_key=True)
     date=db.Column(db.DateTime)
     amount=db.Column(db.Integer)
     team_id=db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
     player_id=db.Column(db.Integer, db.ForeignKey('players.id'), nullable=False)
 
 class Users(db.Model, UserMixin):
-    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
-    created_at=db.Column(db.DateTime, default=datetime.datetime.now())
+    id=db.Column(db.Integer, primary_key=True)
+    created_at=db.Column(db.DateTime, default=datetime.now())
     updated_at=db.Column(db.DateTime, nullable=True)
     username=db.Column(db.String(50),unique=True,nullable=False)
     password=db.Column(db.String(50), nullable=False)
@@ -89,7 +97,7 @@ class Users(db.Model, UserMixin):
     
 class ContactInfo(db.Model):
     __table_args__=(
-         db.CheckConstraint('NOT(phone IS NULL AND email IS NULL)')
+         CheckConstraint('NOT(phone IS NULL AND email IS NULL)'),
     )
 
     id=db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
@@ -98,20 +106,25 @@ class ContactInfo(db.Model):
 
 
 class Managers(Users):
-    id=db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    id=db.Column(db.Integer, primary_key=True)
+    user_id=db.Column(db.Integer,db.ForeignKey('users.id'))
     team_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
         
     def __repr__(self):
         return f"Manager('{self.id}')"
 
-class Player(Users):
-    id=db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+class Players(Users):
+    id=db.Column(db.Integer, primary_key=True)
+    user_id=db.Column(db.Integer,db.ForeignKey('users.id'))
     first_name=db.Column(db.String(50),nullable=False)
     last_name=db.Column(db.String(50),nullable=False)
     date_of_birth=db.Column(db.DateTime,nullable=False)
     status=db.Column(db.Boolean,nullable=False)
     team_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
     #profile_image=db.Column(db.String(20),nullabale=False,default='default.jpg')
+
+    favorite_teams=db.relationship('Favoriteteams', secondary=fans, backref='fans_players')
+    payments=db.relationship('Payments',backref='player', lazy=True)
 
     def __repr__(self):
             return f"Player('{self.first_name}',' ','{self.last_name}')"
